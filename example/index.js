0a58c3432b1f6e8c3636ff1b8ade1a64b3348bb3
Cain <cain@Cain.local>
2017-03-02 15:40:20 +0800

    示例修改

--- example/index.js
+++ example/index.js
@@ -27,7 +27,13 @@ export default class MyCarousel extends React.Component {
       }
     ];
     return (
-      <Carousel data={data} autoplay link='[url]/[id]'/>
+      <Carousel>
+        <div><h3>1</h3></div>
+        <div><h3>2</h3></div>
+        <div><h3>3</h3></div>
+        <div><h3>4</h3></div>
+        <div><h3>5</h3></div>
+      </Carousel>
     );
   }
 };
--- src/index.js
+++ src/index.js
@@ -28,35 +28,46 @@ export default class Carousel extends React.Component {
     render () {
         var thiz = this;
         let props = this.props;
-        let lis = [];
-        this.state.items.forEach((rowData, index) => {
-            if (this.props.link) {
-                var arr = this.props.link.match(/\[[^\]]*?\]/g), href=this.props.link;
-                if (arr) {
-                    arr.forEach(function(val,key){
-                        var arrVal=val.replace("[",'').replace("]",'');
-                        href=href.replace(val,( typeof rowData[arrVal]!='undefined')?rowData[arrVal]:'');
-                    })
-                    lis.push(
-                        <div key={index}><a href={href}><img src={rowData.src}/></a></div>
-                    );
-                }
-            }else {
-                lis.push(
-                    <div key={index} onClick={this.props.click.bind(thiz,index)}><img src={rowData.src}/></div>
-                );
-            }
-        })
-        if (lis.length == 0) {
-            return <a className='loading-carousel'>loading...</a>;
-        }else {
-          return (
-              <div className='f-carousel'>
-                  <SlickCarousel className='slider' {...props} draggable={!props.fade}>
-                      {lis}
-                  </SlickCarousel>
-              </div>
-          );
+        if(this.props.children){
+            return (
+            <div className='f-carousel'>
+                <SlickCarousel className={props.class} {...props} draggable={!props.fade}>
+                    {props.children}
+                </SlickCarousel>
+            </div>
+            )
+        } else {
+          let lis = [];
+          this.state.items.forEach((rowData, index) => {
+              if (this.props.link) {
+                  var arr = this.props.link.match(/\[[^\]]*?\]/g), href=this.props.link;
+                  if (arr) {
+                      arr.forEach(function(val,key){
+                          var arrVal=val.replace("[",'').replace("]",'');
+                          href=href.replace(val,( typeof rowData[arrVal]!='undefined')?rowData[arrVal]:'');
+                      })
+                      lis.push(
+                          <div key={index}><a href={href}><img src={rowData.src}/></a></div>
+                      );
+                  }
+              }else {
+                  lis.push(
+                      <div key={index} onClick={this.props.click.bind(thiz,index)}><img src={rowData.src}/></div>
+                  );
+              }
+          })
+          if (lis.length == 0) {
+              return <a className='loading-carousel'>loading...</a>;
+          }else {
+            return (
+                <div className='f-carousel'>
+                    <SlickCarousel className='slider' {...props} draggable={!props.fade}>
+                        {lis}
+                    </SlickCarousel>
+                </div>
+            );
+          }
         }
+
     }
 }
