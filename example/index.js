cb307fc0f5e6f94fc6af54be5a0917644997a733
Cain <cain@Cain.local>
2017-03-02 15:42:26 +0800

    test example

--- example/index.js
+++ example/index.js
@@ -27,13 +27,7 @@ export default class MyCarousel extends React.Component {
       }
     ];
     return (
-      <Carousel>
-        <div><h3>1</h3></div>
-        <div><h3>2</h3></div>
-        <div><h3>3</h3></div>
-        <div><h3>4</h3></div>
-        <div><h3>5</h3></div>
-      </Carousel>
+      <Carousel data={data} autoplay link='[url]/[id]'/>
     );
   }
 };
